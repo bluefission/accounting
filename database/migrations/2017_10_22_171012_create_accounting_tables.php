524d4e187b18236c3fe24b880470516d3223e7e6
Devon Scott <dscott@bluefission.com>
2017-10-23 03:26:51 -0400

    updated migrations

--- database/migrations/2017_10_22_171012_create_accounting_tables.php
+++ database/migrations/2017_10_22_171012_create_accounting_tables.php
@@ -39,12 +39,28 @@ class CreateAccountingTables extends Migration
             $table->timestamps();
 
         });
+        Schema::create('line_items', function(Blueprint $table) {
+            $table->increments('id');
+            $table->enum('entry_type', ['Debit','Credit']);
+            $table->decimal('amount', 5, 2);
+            $table->string('label')->nullable();
+            $table->string('comment')->nullable();
+            $table->timestamps();
+        });
+
         Schema::create('orders', function(Blueprint $table) {
             $table->increments('id');
-            $table->integer('customer_id')
+            $table->integer('user_id')
                 ->unsigned()
-                ->foreign('customer_id')
+                ->foreign('user_id')
                 ->references('id')->on('customers')
+                ->onDelete('cascade')
+                ->nullable();
+            $table->enum('status', ['Pending','Processing','Done']);
+            $table->integer('line_item_id')
+                ->unsigned()
+                ->foreign('line_item_id')
+                ->references('id')->on('line_items')
                 ->onDelete('cascade');
             $table->timestamps();
         });
@@ -124,6 +140,41 @@ class CreateAccountingTables extends Migration
             $table->string('name');
             $table->timestamps();
         });
+
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')
+                ->unsigned()
+                ->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone');
+
+            $table->integer('address_id')
+                ->unsigned()
+                ->foreign('address_id')
+                ->references('id')->on('addresses')
+                ->onDelete('cascade');
+
+            $table->integer('role_id')
+                ->unsigned()
+                ->foreign('role_id')
+                ->references('id')->on('roles')
+                ->onDelete('cascade');
+
+            $table->integer('user_type_id')
+                ->unsigned()
+                ->nullable()
+                ->foreign('user_type_id')
+                ->references('id')->on('user_types')
+                ->onDelete('cascade');
+            
+            $table->string('entitlements')
+                ->nullable();
+            $table->timestamps();
     }
 
     /**
@@ -138,11 +189,14 @@ class CreateAccountingTables extends Migration
         Schema::dropIfExists('customers');
         Schema::dropIfExists('orders');
         Schema::dropIfExists('items');
+        Schema::dropIfExists('line_item');
         Schema::dropIfExists('order_items');
         Schema::dropIfExists('products');
         Schema::dropIfExists('services');
         Schema::dropIfExists('settings');
         Schema::dropIfExists('user_types');
         Schema::dropIfExists('roles');
+        Schema::dropIfExists('accounts');
+
     }
 }
